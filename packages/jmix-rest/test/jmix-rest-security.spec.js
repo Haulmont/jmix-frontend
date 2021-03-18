4bad945e2673ea67b40e52c222cc64d1b2116478
Vadim Basko <vadim.p.basko@gmail.com>
2021-03-18 15:11:21 +0400

    chore: fix tests after rest api update

--- packages/jmix-rest/test/jmix-rest-security.spec.js
+++ packages/jmix-rest/test/jmix-rest-security.spec.js
@@ -7,10 +7,11 @@ global.fetch = require('node-fetch');
 describe('JmixRestConnection security methods', () => {
 
   let app;
+  const loginOpts = {tokenEndpoint: 'http://localhost:8080/oauth/token'};
 
   it('should load effective perms for admin', async () => {
     app = initApp();
-    await app.login('admin', 'admin');
+    await app.login('admin', 'admin', loginOpts);
 
     const perms = await app.getEffectivePermissions();
     const carEntityPerms = perms.entities.filter(item => item.target.startsWith('scr$Car:'));
@@ -35,7 +36,7 @@ describe('JmixRestConnection security methods', () => {
   it('should load effective perms for mechanic', async () => {
 
     app = initApp();
-    await app.login('mechanic', '1');
+    await app.login('mechanic', '1', loginOpts);
     const perms = await app.getEffectivePermissions();
     const carEntityPerms = perms.entities.filter(item => item.target.startsWith('scr$Car:'));
     const carEntityAttrPerms = perms.entityAttributes.filter(item => item.target.startsWith('scr$Car:'));
@@ -66,7 +67,7 @@ describe('JmixRestConnection security methods', () => {
   it('should load effective perms for manager', async () => {
 
     app = initApp();
-    await app.login('manager', '2');
+    await app.login('manager', '2', loginOpts);
     const perms = await app.getEffectivePermissions();
     const carEntityPerms = perms.entities.filter(item => item.target.startsWith('scr$Car:'));
     const carEntityAttrPerms = perms.entityAttributes.filter(item => item.target.startsWith('scr$Car:'));
