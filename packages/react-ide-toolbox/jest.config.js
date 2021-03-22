9aba7d1a4d415e35d3df8988a331d5e0a7bb0eb7
Vlad Minaev <minaev@haulmont.com>
2021-03-22 11:57:53 +0400

    chore: add jest config

---
+++ packages/react-ide-toolbox/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "coverageThreshold": {
+    "global": {
+      "statements": 100,
+      "branches": 100,
+      "functions": 100,
+      "lines": 100
+    }
+  },
+  "collectCoverageFrom": [
+    "src/**/*.{ts,tsx}",
+  ]
+};
