33f1afa3596abd0cb19321e480f3859b26772976
Roman Karandashov <r.karandashov@haulmont.com>
2021-03-25 19:56:13 +0400

    fix: fixed issue with string id questions #179
    affects: @haulmont/jmix-front-generator

--- packages/jmix-front-generator/src/generators/react-typescript/entity-management/answers.ts
+++ packages/jmix-front-generator/src/generators/react-typescript/entity-management/answers.ts
@@ -3,29 +3,28 @@ import {ProjectModel, View} from "../../../common/model/cuba-model";
 import {YeomanGenerator} from "../../../building-blocks/YeomanGenerator";
 import {askQuestions} from "../../../building-blocks/stages/answers/defaultGetAnswersFromPrompt";
 import {EntityWithPath} from "../../../building-blocks/stages/template-model/pieces/entity";
-import {stringIdQuestions} from "../../../building-blocks/stages/answers/pieces/stringId";
+import {askStringIdQuestions, StringIdAnswers} from "../../../building-blocks/stages/answers/pieces/stringId";
 import {StudioTemplateProperty} from "../../../common/studio/studio-model";
 import {ListTypes} from "../../../building-blocks/stages/template-model/pieces/entity-management/list-types";
 import {commonEntityManagementQuestions, viewQuestions} from "../../../building-blocks/stages/answers/pieces/entity-management/entity-management-common";
+import { isStringIdEntity } from "../common/entity";
 
-export type Answers = {
-    entity: EntityWithPath;
-    managementComponentName: string;
-    listType: ListTypes;
-    listView: View;
-    listComponentName: string;
-    listAttributes: string;
-    editComponentName: string;
-    editAttributes: string;
-    editView: View;
-    nestedEntityInfo?: Record<string, string>;
-    idAttrName?: string;
+export interface Answers extends StringIdAnswers {
+    entity: EntityWithPath,
+    managementComponentName: string,
+    listType: ListTypes,
+    listView: View,
+    listComponentName: string,
+    listAttributes: string,
+    editComponentName: string,
+    editAttributes: string,
+    editView: View,
+    nestedEntityInfo?: Record<string, string>
   }
 
   export const allQuestions: StudioTemplateProperty[] = [
     ...commonEntityManagementQuestions,
-    ...viewQuestions,
-    ...stringIdQuestions
+    ...viewQuestions
   ];
 
   export const getAnswersFromPrompt = async (
@@ -36,7 +35,14 @@ export type Answers = {
       ...commonEntityManagementQuestions,
       ...viewQuestions
     ];
-    let answers = await askQuestions<Answers>(initialQuestions, projectModel, gen);
-
-    return answers;
+    let answers: Answers = await askQuestions<Answers>(initialQuestions, projectModel, gen);
+    let stringIdAnswers: StringIdAnswers = isStringIdEntity(projectModel, answers.entity) 
+    ? await askStringIdQuestions(
+        answers.entity,
+        projectModel,
+        gen
+      )
+    : {}
+  
+    return {...answers, ...stringIdAnswers};
   }
