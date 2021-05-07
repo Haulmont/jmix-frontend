8246137857ed98410166c46e2922c22f994fb8f6
Vlad Minaev <minaev@haulmont.com>
2021-05-07 17:04:57 +0400

    feat: basic components palette in react app template #270
    affects: @haulmont/jmix-front-generator, @haulmont/react-ide-toolbox

--- packages/react-ide-toolbox/src/index.ts
+++ packages/react-ide-toolbox/src/index.ts
@@ -2,3 +2,5 @@ export * from "./previews/dev-support";
 export * from "./previews/previews";
 export * from "./previews/component-preview";
 export * from "./previews/previews.types";
+
+export * from "./palette/palette"
