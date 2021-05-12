f18133ba359bcae40d988a747f3fc888fe73e6a0
Vlad Minaev <minaev@haulmont.com>
2021-05-12 22:18:02 +0400

    WIP: template icon
    affects: @haulmont/jmix-front-generator

--- packages/jmix-front-generator/scripts/copy-templates.js
+++ packages/jmix-front-generator/scripts/copy-templates.js
@@ -3,5 +3,6 @@ const vfs = require('vinyl-fs');
 vfs.src([
   'src/**/template/**',
   'src/**/template/.**',
-  'src/**/info.json']
+  'src/**/info.json',
+  'src/**/*.svg']
 ).pipe(vfs.dest('lib'));
