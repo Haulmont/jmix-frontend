2b5c4415bb4906b565774d1f9acbc27d7624da40
Roman Karandashov <r.karandashov@haulmont.com>
2021-05-31 16:54:17 +0400

    feat: adding MenuItem to SubMenuItem child by key #333
    affects: @haulmont/jmix-front-generator

--- packages/jmix-front-generator/src/test/generators/react-typrscript/common/menu.test.ts
+++ packages/jmix-front-generator/src/test/generators/react-typrscript/common/menu.test.ts
@@ -3,42 +3,178 @@ import uuid from "uuid";
 import {assert} from 'chai'
 
 describe ('AppMenu generation', () => {
-  it('test getNewMenuItem', () => {
-    const key = uuid.v4();
-    const componentName = "TestComponent";
+  describe('new menu item generation', () => {
+    it('test getNewMenuItem', () => {
+      const key = uuid.v4();
+      const componentName = "TestComponent";
+  
+      const expectedMenuItem  = `
+      <MenuItem 
+        screenId={"${componentName}"}
+        icon={<BarsOutlined />}
+        caption={<FormattedMessage id={"router.${componentName}"} />}
+        key={'${key}'}
+      />`;
+  
+      assert.equal(getNewMenuItem(componentName, key), expectedMenuItem);
+    });
+  });
 
-    const expectedMenuItem  = `
-    <MenuItem 
-      screenId={"${componentName}"}
-      icon={<BarsOutlined />}
-      caption={<FormattedMessage id={"router.${componentName}"} />}
-      key={'${key}'}
-    />`;
+  describe("update contentn in AppMenu", () => {
+    describe("AppMenu with VerticalMenu", () => {
+      it('adding new menu item to ROOT node', () => {
+        const key = uuid.v4();
+        const componentName = "TestComponent";
+        const menuNode = "ROOT";
+        const appMenuContent = `
+        <VerticalMenu>
+          <MenuItem/>
+        </VerticalMenu>
+        `;
+        const expectedUpdatedMenuContent = `\r
+        <VerticalMenu>\r
+          <MenuItem/>\r
+          <MenuItem \r
+                screenId={"${componentName}"}\r
+                icon={<BarsOutlined />}\r
+                caption={<FormattedMessage id={"router.${componentName}"} />}\r
+                key={'${key}'}\r
+              /></VerticalMenu>\r
+        `;
+    
+        assert.equal(updateAppMenuContent(appMenuContent, componentName, menuNode, key), expectedUpdatedMenuContent);
+      });
 
-    assert.equal(getNewMenuItem(componentName, key), expectedMenuItem);
-  })
+      it('adding new menu item to custom node', () => {
+        const key = uuid.v4();
+        const componentName = "TestComponent";
+        const menuNode = "test";
+        const appMenuContent = `
+        <VerticalMenu>
+          <MenuItem/>
+          <SubMenuItem
+                key={"test"}
+          >
+            <MenuItem/>
+          </SubMenuItem>
+          <MenuItem/>
+        </VerticalMenu>
+        `;
+        const expectedUpdatedMenuContent = `\r
+        <VerticalMenu>\r
+          <MenuItem/>\r
+          <SubMenuItem\r
+                key={"test"}\r
+          >\r
+            <MenuItem/>\r
+            <MenuItem \r
+                  screenId={"${componentName}"}\r
+                  icon={<BarsOutlined />}\r
+                  caption={<FormattedMessage id={"router.${componentName}"} />}\r
+                  key={'${key}'}\r
+                /></SubMenuItem>\r
+          <MenuItem/>\r
+        </VerticalMenu>\r
+        `;
 
-  it('test updateAppMenuContent', () => {
-    const key = uuid.v4();
-    const componentName = "TestComponent";
-    const menuNode = "ROOT";
-    const appMenuContent = `
-    <VerticalMenu>
-      <MenuItem/>
-    </VerticalMenu>
-    `;
-    const expectedUpdatedMenuContentn = `
-    <VerticalMenu>
-      <MenuItem/>
-     
-    <MenuItem 
-      screenId={"${componentName}"}
-      icon={<BarsOutlined />}
-      caption={<FormattedMessage id={"router.${componentName}"} />}
-      key={'${key}'}
-    /></VerticalMenu>
-    `;
+        assert.equal(updateAppMenuContent(appMenuContent, componentName, menuNode, key), expectedUpdatedMenuContent);
+      });
 
-    assert.equal(updateAppMenuContent(appMenuContent, componentName, menuNode, key), expectedUpdatedMenuContentn);
+      it("updating doesn't happen when menu node is null", () => {
+        const key = uuid.v4();
+        const componentName = "TestComponent";
+        const appMenuContent = `
+        <VerticalMenu>
+          <MenuItem/>
+          <SubMenuItem
+                key={"test"}
+          >
+            <MenuItem/>
+          </SubMenuItem>
+          <MenuItem/>
+        </VerticalMenu>
+        `;
+
+        assert.equal(updateAppMenuContent(appMenuContent, componentName, null, key), appMenuContent);
+      })
+    })
+
+    describe("AppMenu with HorizontalMenu", () => {
+      it('adding new menu item to ROOT node', () => {
+        const key = uuid.v4();
+        const componentName = "TestComponent";
+        const menuNode = "ROOT";
+        const appMenuContent = `
+        <HorizontalMenu>
+          <MenuItem/>
+        </HorizontalMenu>
+        `;
+        const expectedUpdatedMenuContent = `\r
+        <HorizontalMenu>\r
+          <MenuItem/>\r
+          <MenuItem \r
+                screenId={"${componentName}"}\r
+                icon={<BarsOutlined />}\r
+                caption={<FormattedMessage id={"router.${componentName}"} />}\r
+                key={'${key}'}\r
+              /></HorizontalMenu>\r
+        `;
+    
+        assert.equal(updateAppMenuContent(appMenuContent, componentName, menuNode, key), expectedUpdatedMenuContent);
+      });
+
+      it('adding new menu item to custom node', () => {
+        const key = uuid.v4();
+        const componentName = "TestComponent";
+        const menuNode = "test";
+        const appMenuContent = `
+        <HorizontalMenu>
+          <MenuItem/>
+          <SubMenuItem
+                key={"test"}
+          >
+            <MenuItem/>
+          </SubMenuItem>
+          <MenuItem/>
+        </HorizontalMenu>
+        `;
+        const expectedUpdatedMenuContent = `\r
+        <HorizontalMenu>\r
+          <MenuItem/>\r
+          <SubMenuItem\r
+                key={"test"}\r
+          >\r
+            <MenuItem/>\r
+            <MenuItem \r
+                  screenId={"${componentName}"}\r
+                  icon={<BarsOutlined />}\r
+                  caption={<FormattedMessage id={"router.${componentName}"} />}\r
+                  key={'${key}'}\r
+                /></SubMenuItem>\r
+          <MenuItem/>\r
+        </HorizontalMenu>\r
+        `;
+
+        assert.equal(updateAppMenuContent(appMenuContent, componentName, menuNode, key), expectedUpdatedMenuContent);
+      });
+
+      it("updating doesn't happen when menu node is null", () => {
+        const key = uuid.v4();
+        const componentName = "TestComponent";
+        const appMenuContent = `
+        <HorizontalMenu>
+          <MenuItem/>
+          <SubMenuItem
+                key={"test"}
+          >
+            <MenuItem/>
+          </SubMenuItem>
+          <MenuItem/>
+        </HorizontalMenu>
+        `;
+
+        assert.equal(updateAppMenuContent(appMenuContent, componentName, null, key), appMenuContent);
+      })
+    })
   })
 })
