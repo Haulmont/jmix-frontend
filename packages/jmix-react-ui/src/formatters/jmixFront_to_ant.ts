9e5c66e396b74e09c654725b9737714c0ff546d1
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2021-06-03 19:11:46 +0400

    refactor: jmixFront_to_ant - simplify, restructure, add error reporting
    affects: @haulmont/jmix-react-ui

--- packages/jmix-react-ui/src/formatters/jmixFront_to_ant.ts
+++ packages/jmix-react-ui/src/formatters/jmixFront_to_ant.ts
@@ -1,126 +1,150 @@
 import {
-  instanceItemToFormFields,
   Metadata,
   getDataTransferFormat,
   getPropertyInfo,
   isFileProperty,
   isOneToManyComposition,
   isOneToOneComposition,
-  isRelationProperty,
   isTemporalProperty,
   isToManyAssociation,
   isToOneAssociation,
-  WithId,
+  HasId,
   TemporalPropertyType,
-  toIdString
+  toIdString,
+  EntityInstance
 } from "@haulmont/jmix-react-core";
-import dayjs from "dayjs";
+import dayjs  from "dayjs";
 import { toJS } from "mobx";
 
 /**
  * Reformats the entity data so that it can be
  * properly displayed by Ant Design `<Form>`.
  *
- * @typeparam T - entity type.
- *
- * @param item
+ * @param entityInstance
  * @param entityName
  * @param metadata
  * @param stringIdName
  */
-export function jmixFront_to_ant<T>(
-  item: Record<string, any>,
+export function jmixFront_to_ant(
+  entityInstance: EntityInstance,
   entityName: string,
   metadata: Metadata,
   stringIdName?: string
 ): Record<string, any> {
-  if (item == null || metadata == null) {
+  if (entityInstance == null || metadata == null) {
     return {};
   }
 
   const fields: Record<string, any> = {};
 
-  Object.entries(toJS(item)).forEach(([key, value]) => {
-    const propInfo = getPropertyInfo(metadata, entityName, key);
+  Object.entries(toJS(entityInstance)).forEach(([attrName, value]) => {
+    const propInfo = getPropertyInfo(metadata.entities, entityName, attrName);
 
-    const isStringIdAttr: boolean = (stringIdName != null) && (key === 'id');
+    if (propInfo == null) {
+      console.error(`Cannot find property info for attribute ${attrName} of entity ${entityName}`);
+      fields[attrName] = value;
+      return;
+    }
 
+    // String ID attribute
+    const isStringIdAttr: boolean = (stringIdName != null) && (attrName === 'id');
     if (isStringIdAttr) {
       fields[stringIdName!] = value;
       return;
     }
 
-    if (propInfo == null) {
-      fields[key] = value;
-      return;
-    }
-
     if(isOneToOneComposition(propInfo)) {
-      if (value != null) {
-        fields[key] = instanceItemToFormFields(value, propInfo.type, metadata);
+      if (value == null) {
+        // We need to explicitly set `null` on empty fields rather than just omit the key.
+        // Example of why things can go wrong otherwise: https://github.com/Haulmont/jmix-frontend/issues/318.
+        // What happens is that the form fields will be set twice, first with data from Apollo cache and then from network response.
+        // If cache contains a value and network response says that the field should be empty, then unless we explicitly set `null`
+        // during the second call of `setFieldsValue`, the form will still contain the old value.
+        fields[attrName] = null;
         return;
       }
 
-      // We need to explicitly set `null` on empty fields rather than just omit the key.
-      // Example of why things can go wrong otherwise: https://github.com/Haulmont/jmix-frontend/issues/318.
-      // What happens is that the form fields will be set twice, first with data from Apollo cache and then from network response.
-      // If cache contains a value and network response says that the field should be empty, then unless we explicitly set `null`
-      // during the second call of `setFieldsValue`, the form will still contain the old value.
-      fields[key] = null;
+      if (typeof value === 'object') {
+        // Recursively traverse child entity
+        fields[attrName] = jmixFront_to_ant(value, propInfo.type, metadata);
+        return;
+      }
+
+      console.error('Expected an object:', value);
       return;
     }
 
     if (isOneToManyComposition(propInfo)) {
-      value == null
-        ? fields[key] = []
-        : fields[key] = value.map((e: T) => instanceItemToFormFields(e, propInfo.type, metadata));
+      if (value == null) {
+        fields[attrName] = [];
+        return;
+      }
+
+      // Recursively traverse child entities
+      if (Array.isArray(value)) {
+        fields[attrName] = value.map((e: EntityInstance) => jmixFront_to_ant(e, propInfo.type, metadata));
+        return;
+      }
+
+      console.error('Expected an array:', value);
       return;
     }
 
     if (isToManyAssociation(propInfo)) {
       if (value == null) {
-        fields[key] = [];
+        fields[attrName] = [];
         return;
       }
 
-      const entityList = value as unknown as WithId[];
-      fields[key] = entityList.reduce<string[]>((accumulator, nextEntity) => {
-        accumulator.push(toIdString(nextEntity.id!));
-        return accumulator;
-      }, []);
-      return;
-    }
+      // Turn array of EntityInstance into array of ids
+      if (Array.isArray(value)) {
+        fields[attrName] = value.reduce<string[]>((arrayOfIds, entity) => {
+          arrayOfIds.push(toIdString(entity.id));
+          return arrayOfIds;
+        }, []);
+        return;
+      }
 
-    if (isRelationProperty(propInfo) && value == null) {
-      fields[key] = value;
-      return;
-    }
-
-    if (isFileProperty(propInfo)) {
-      fields[key] = value; // FileRef string
+      console.error('Expected an array:', value);
       return;
     }
 
     if (isToOneAssociation(propInfo)) {
-      fields[key] = (value as WithId).id!;
+      if (value == null) {
+        fields[attrName] = null;
+        return;
+      }
+
+      if ((value as HasId).id != null) {
+        fields[attrName] = (value as HasId).id;
+        return;
+      }
+
+      console.error('Expected to have an id:', value);
+      return;
+    }
+
+    if (isFileProperty(propInfo)) {
+      fields[attrName] = value; // FileRef string
       return;
     }
 
     if (isTemporalProperty(propInfo)) {
-      if (value != null) {
-        fields[key] = dayjs(value, getDataTransferFormat(propInfo.type as TemporalPropertyType));
-      } else {
-        fields[key] = null;
+      if (value == null) {
+        fields[attrName] = null;
+        return;
       }
+
+      if (dayjs.isDayjs(value)) {
+        fields[attrName] = dayjs(value, getDataTransferFormat(propInfo.type as TemporalPropertyType));
+        return;
+      }
+
+      console.error('Expected to be a dayjs object:', value);
       return;
     }
 
-    if (value == null) {
-      fields[key] = null;
-      return;
-    }
-
-    fields[key] = value;
+    fields[attrName] = value;
     return;
   });
 
