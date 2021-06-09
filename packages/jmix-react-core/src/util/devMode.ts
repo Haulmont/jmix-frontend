09a7fed7fa535a13d692a936aa1c5d6e0a8daf05
Vlad Minaev <minaev@haulmont.com>
2021-06-09 21:15:58 +0400

    chore: bypass permissions for dev mode #216

---
+++ packages/jmix-react-core/src/util/devMode.ts
@@ -0,0 +1,8 @@
+// It's more correct to retrieve dev mode status from @haulmont/react-ide-devtools
+// but this will be unnecessary once https://github.com/Haulmont/jmix-frontend/issues/385 is resolved.
+
+const DEV_MODE = process.env.REACT_APP_IDE_DEVMODE === "true";
+
+export const isDevModeEnabled = () => {
+  return DEV_MODE;
+}
