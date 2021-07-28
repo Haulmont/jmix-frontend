b7ddf13a34b3087062b2404d5a084e8d3c931d14
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2021-07-28 15:30:34 +0400

    WIP: front-generator compilation

--- example-react-app/generator-customization/generators/copy-templates.js
+++ example-react-app/generator-customization/generators/copy-templates.js
@@ -1,7 +1,8 @@
 const vfs = require('vinyl-fs');
+const path = require('path');
 
 vfs.src([
-  'src/**/template/**',
-  'src/**/template/.**',
-  'src/**/*.svg']
-).pipe(vfs.dest('build'));
+  path.resolve(__dirname, 'src/**/template/**'),
+  path.resolve(__dirname, 'src/**/template/.**'),
+  path.resolve(__dirname, 'src/**/*.svg')
+]).pipe(vfs.dest(path.resolve(__dirname, 'build')));
