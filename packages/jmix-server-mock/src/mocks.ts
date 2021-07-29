7c0911681f7ee91404a3388c28585750e4e9f63b
Vadim Basko <vadim.p.basko@gmail.com>
2021-07-29 10:48:30 +0400

    chore: mock server data improvements

--- packages/jmix-server-mock/src/mocks.ts
+++ packages/jmix-server-mock/src/mocks.ts
@@ -1,6 +1,8 @@
+import * as casual from "casual";
+
 export const mocks = {
   String: () => 'Banana',
-  UUID: () => '123e4567-e89b-12d3-a456-426614174000',
+  UUID: () => casual._uuid(),
   Long: () => '2',
   BigDecimal: () => '1',
   BigInteger: () => '1',
@@ -11,7 +13,8 @@ export const mocks = {
   OffsetDateTime: () => '2021-01-01T23:59:59+04:00',
   LocalDate: () => '2020-03-03',
   LocalDateTime: () => '2020-03-03T03:03:03',
-  LocalTime: () => '23:59:59'
+  LocalTime: () => '23:59:59',
+  Char: () => 'A'
 }
 
 export function mockedResolvers(document) {
