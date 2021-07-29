84963ec4255231840f9caf4c6eb5dd75a242c199
Vadim Basko <vadim.p.basko@gmail.com>
2021-07-29 16:00:16 +0400

    chore: restore rest tests

--- packages/jmix-rest/test/jmix-rest-not-logged.spec.js
+++ packages/jmix-rest/test/jmix-rest-not-logged.spec.js
@@ -1,14 +1,15 @@
 "use strict";
 
+const assert = require("assert");
 const {initApp} = require('./common');
 global.fetch = require('node-fetch');
 
 describe('Jmix not logged in', function() {
-    it('.setSessionLocale() should fail if not logged in', function(done) {
+    it('.getEffectivePermissions() should fail if not logged in', function(done) {
       const app = initApp();
-      app.setSessionLocale()
+      app.getEffectivePermissions()
         .then(() => {
-          done('did not fail');
+          assert(false);
         })
         .catch(() => {
           done();
