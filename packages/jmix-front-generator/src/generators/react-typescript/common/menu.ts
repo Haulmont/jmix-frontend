1439044db1de8081c2a22650308612e92c34e87e
Святовец Денис Олегович <DOSvyatovets@sberbank.ru>
2021-08-23 20:21:05 +0300

    feat: support config for generating app menu items #578
    affects: @haulmont/jmix-front-generator

--- packages/jmix-front-generator/src/generators/react-typescript/common/menu.ts
+++ packages/jmix-front-generator/src/generators/react-typescript/common/menu.ts
@@ -4,6 +4,14 @@ import uuid from 'uuid'
 import {convertToUnixPath, unCapitalizeFirst} from "../../../common/utils";
 import jscodeshift, {JSXElement, JSXAttribute, stringLiteral, Collection } from 'jscodeshift'
 
+export interface AddMenuItemConfig {
+  horizontalMenuComponentName?: string;
+  verticalMenuComponentName?: string;
+  subMenuItemComponentName?: string;
+  getMenuItem?: (componentClassName: string, key: string) => string;
+  getSubMenuItem?: (caption: string, key: string, children?: string) => string;
+}
+
 interface AddToMenuOpts {
   destRoot: string;
   dirShift: string | undefined;
@@ -102,45 +110,10 @@ export function addAddonToMenu (
   }
 }
 
-export function addToAppMenu<T>(
-  fs: Generator.MemFsEditor,
-  {
-    destRoot,
-    dirShift,
-    menuNode,
-    componentClassName,
-    key,
-    menuItemType
-  }: AddToMenuOpts & AppMenuItemInfo,
-  addAppMenuCallback: (
-    appMenuContents: string,
-    appMenuItemInfo: AppMenuItemInfo,
-    customComponentParams?: {[param: string]: unknown}
-  ) => string = addAppMenuItem
-): boolean {
-
-  const appMenuDir = path.join(destRoot, dirShift ? dirShift : '');
-  const appMenuPath = path.join(appMenuDir, 'app', 'AppMenu.tsx');
-  
-  if (fs.exists(appMenuPath)) {
-    const appMenuContents = fs.read(appMenuPath);
-    fs.write(
-      appMenuPath,
-      addAppMenuCallback(
-        appMenuContents,
-        {componentClassName, menuNode, key, menuItemType}
-      ));
-    return true;
-  } else {
-    return false;
-  }
-}
-
 export type RouteInfo = ComponentInfo & {
   componentPath: string,
   pathPattern: string
 }
-
 
 export const addRoute = (routingContents: string,
                   {
@@ -171,32 +144,40 @@ export const addAppMenuItem = (appMenuContents: string,
     menuNode,
     key,
     menuItemType
-  }: AppMenuItemInfo) => {
+  }: AppMenuItemInfo,
+  config: AddMenuItemConfig) => {
 
     return updateAppMenuContent(
       appMenuContents, 
       componentClassName, 
       menuNode,
       key ?? uuid.v4(),
-      menuItemType
+      menuItemType,
+      config
     );
 
   }
 
-function getUpdatedAppMenuContent(appMenuContents: string, menuNode: string, componentClassName: string, menuItemType: MenuItemTypes, key: string): string {
-
+function getUpdatedAppMenuContent(
+  appMenuContents: string,
+  menuNode: string,
+  componentClassName: string,
+  menuItemType: MenuItemTypes,
+  key: string,
+  config?: AddMenuItemConfig
+): string {
   const tsxParser = jscodeshift.withParser('tsx');
   const appMenuAST = tsxParser(appMenuContents);
 
-  const newMenuItem = getNewMenuItem(componentClassName, key, menuItemType);
+  const newMenuItem = getNewMenuItem(componentClassName, key, menuItemType, '', config);
 
   if(menuNode === "ROOT") {
-    return getUpdatedMenuWithRootNode(appMenuAST, newMenuItem);
+    return getUpdatedMenuWithRootNode(appMenuAST, newMenuItem, config);
   }
   if(menuNode === "ADDON") {
-    return getUpdatedMenuWithAddonNode(appMenuAST, newMenuItem);
+    return getUpdatedMenuWithAddonNode(appMenuAST, newMenuItem, config);
   }
-  return getUpdatedMenuWithCustomNode(appMenuAST, menuNode, newMenuItem);
+  return getUpdatedMenuWithCustomNode(appMenuAST, menuNode, newMenuItem, config);
 }
 
 export function updateAppMenuContent(
@@ -205,6 +186,7 @@ export function updateAppMenuContent(
   menuNode: string | null,
   key: string ,
   menuItemType: MenuItemTypes = MenuItemTypes.MenuItem,
+  config?: AddMenuItemConfig
 ) {
   return menuNode 
     ?  getUpdatedAppMenuContent(
@@ -212,7 +194,8 @@ export function updateAppMenuContent(
          menuNode, 
          componentClassName,
          menuItemType,
-         key
+         key,
+         config
       )
     : appMenuContents
 }
@@ -221,11 +204,12 @@ export function getNewMenuItem(
   componentClassName: string, 
   key: string, 
   menuItemType: MenuItemTypes,
-  children?: string
+  children?: string,
+  config?: AddMenuItemConfig
 ) : string {
   switch(menuItemType) {
-    case MenuItemTypes.MenuItem: return getMenuItem(componentClassName, key);
-    case MenuItemTypes.SubMenuItem: return getSubMenuItem(componentClassName, key, children);
+    case MenuItemTypes.MenuItem: return (config?.getMenuItem ?? getMenuItem)(componentClassName, key);
+    case MenuItemTypes.SubMenuItem: return (config?.getSubMenuItem ?? getSubMenuItem)(componentClassName, key, children);
   }
 }
 
@@ -260,13 +244,17 @@ export function pushStringNodeToParentChildren(parrent: Collection<JSXElement>, 
   return parrent.toSource();
 }
 
-export function getUpdatedMenuWithRootNode(appMenuAST: Collection<any>, newMenuItem: string) : string {
-  const verticalMenu = appMenuAST.findJSXElements('VerticalMenu');
+export function getUpdatedMenuWithRootNode(
+  appMenuAST: Collection<any>, 
+  newMenuItem: string,
+  config?: AddMenuItemConfig
+) : string {
+  const verticalMenu = appMenuAST.findJSXElements(config?.verticalMenuComponentName ?? 'VerticalMenu');
   if(verticalMenu.length){
     return pushStringNodeToParentChildren(verticalMenu, newMenuItem);
   }
 
-  const horizontalMenu = appMenuAST.findJSXElements('HorizontalMenu');
+  const horizontalMenu = appMenuAST.findJSXElements(config?.horizontalMenuComponentName ?? 'HorizontalMenu');
   if(horizontalMenu.length){
     return pushStringNodeToParentChildren(horizontalMenu, newMenuItem);
   }
@@ -274,9 +262,14 @@ export function getUpdatedMenuWithRootNode(appMenuAST: Collection<any>, newMenuI
   return appMenuAST.toSource();
 }
 
-export function getUpdatedMenuWithCustomNode(appMenuAST: Collection<any>, menuNode: string, newMenuItem: string) : string {
+export function getUpdatedMenuWithCustomNode(
+  appMenuAST: Collection<any>,
+  menuNode: string,
+  newMenuItem: string,
+  config?: AddMenuItemConfig
+) : string {
   const parrentSubMenuItem = appMenuAST
-    .findJSXElements("SubMenuItem")
+    .findJSXElements(config?.subMenuItemComponentName ?? "SubMenuItem")
     .find(JSXAttribute, {
       name: {
         type: "JSXIdentifier",
@@ -295,10 +288,14 @@ export function getUpdatedMenuWithCustomNode(appMenuAST: Collection<any>, menuNo
     return pushStringNodeToParentChildren(parrentSubMenuItem, newMenuItem);
 }
 
-export function getUpdatedMenuWithAddonNode(appMenuAST: Collection<any>, newMenuItem: string) : string {
-  let parrentNode = findSubMenuItemByKey(appMenuAST, 'addons');
+export function getUpdatedMenuWithAddonNode(
+  appMenuAST: Collection<any>,
+  newMenuItem: string,
+  config?: AddMenuItemConfig
+) : string {
+  let parrentNode = findSubMenuItemByKey(appMenuAST, 'addons', config);
   if(parrentNode.length === 0) {
-    parrentNode = findRootMenuNode(appMenuAST);
+    parrentNode = findRootMenuNode(appMenuAST, config);
     const addonSubMenuItem = getSubMenuItem('addons.Addons', 'addons', newMenuItem);
     return pushStringNodeToParentChildren(parrentNode, addonSubMenuItem);
   }
@@ -306,9 +303,9 @@ export function getUpdatedMenuWithAddonNode(appMenuAST: Collection<any>, newMenu
     return pushStringNodeToParentChildren(parrentNode, newMenuItem);
 }
 
-function findSubMenuItemByKey(appMenuAST: Collection<any> ,key: string): Collection<JSXElement> {
+function findSubMenuItemByKey(appMenuAST: Collection<any> ,key: string, config?: AddMenuItemConfig): Collection<JSXElement> {
   return appMenuAST
-    .findJSXElements("SubMenuItem")
+    .findJSXElements(config?.subMenuItemComponentName ?? "SubMenuItem")
     .find(JSXAttribute, {
       name: {
         type: "JSXIdentifier",
@@ -325,12 +322,12 @@ function findSubMenuItemByKey(appMenuAST: Collection<any> ,key: string): Collect
     .closest(JSXElement);
 }
 
-function findRootMenuNode(appMenuAST: Collection<any>): Collection<JSXElement> {
-  const verticalMenu = appMenuAST.findJSXElements('VerticalMenu');
+function findRootMenuNode(appMenuAST: Collection<any>, config?: AddMenuItemConfig): Collection<JSXElement> {
+  const verticalMenu = appMenuAST.findJSXElements(config?.verticalMenuComponentName ?? 'VerticalMenu');
   if(verticalMenu.length){
     return verticalMenu;
   }
-  return appMenuAST.findJSXElements('HorizontalMenu');
+  return appMenuAST.findJSXElements(config?.horizontalMenuComponentName ?? 'HorizontalMenu');
 }
 
 function getRelativePath(routingDir: string, destRoot: string) {
