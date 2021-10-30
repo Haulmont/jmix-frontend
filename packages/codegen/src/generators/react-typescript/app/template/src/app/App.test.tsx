421d0db89b62e75815ef2e1827b92b7280ab0598
Vadim Basko <vadim.p.basko@gmail.com>
2021-10-30 12:14:39 +0400

    test: add example-app test

---
+++ packages/codegen/src/generators/react-typescript/app/template/src/app/App.test.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache
+} from "@apollo/client";
+import en from "../i18n/en.json";
+import {
+  HotkeyContext,
+  HotkeyStore,
+  ScreenContext,
+  Screens
+} from "@amplicode/react-core";
+import { defaultHotkeyConfigs } from "../hotkeyConfigs";
+import { GRAPHQL_URI } from "../config";
+import { onError } from "@apollo/client/link/error";
+import { act } from "react-dom/test-utils";
+import { securityStore } from "../security-store";
+
+it("renders without crashing", () => {
+  const screens = new Screens();
+
+  const hotkeys = new HotkeyStore(defaultHotkeyConfigs);
+
+  const httpLink = createHttpLink({
+    uri: GRAPHQL_URI,
+    credentials: "same-origin"
+  });
+
+  const logoutLink = onError(({ networkError }) => {
+    if (networkError == null || !("statusCode" in networkError)) {
+      return;
+    }
+    if (networkError.statusCode === 401) {
+      securityStore.logout();
+    }
+  });
+
+  const client = new ApolloClient({
+    link: logoutLink.concat(httpLink),
+    cache: new InMemoryCache({
+      addTypename: false
+    }),
+    defaultOptions: {
+      query: {
+        fetchPolicy: "network-only"
+      },
+      watchQuery: {
+        fetchPolicy: "cache-and-network"
+      }
+    }
+  });
+
+  const div = document.createElement("div");
+
+  act(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <ApolloProvider client={client}>
+          <IntlProvider locale="en" messages={en}>
+            <ScreenContext.Provider value={screens}>
+              <HashRouter>
+                <HotkeyContext.Provider value={hotkeys}>
+                  <App />
+                </HotkeyContext.Provider>
+              </HashRouter>
+            </ScreenContext.Provider>
+          </IntlProvider>
+        </ApolloProvider>
+      </React.StrictMode>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
