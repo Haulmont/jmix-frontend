c7f658a814831c8d2a0f2f5489ab58b561fbef3c
Ilya Trifonov <i.trifonov@haulmont.com>
2021-11-11 11:36:54 +0300

    feat: add TextArea form control
    affects: @haulmont/jmix-react-antd, test-puppeteer

---
+++ packages/jmix-react-antd/src/ui/form/controls/TextArea.tsx
@@ -0,0 +1,21 @@
+import { Input } from "antd";
+import * as React from "react";
+import {JmixFormFieldWrapper} from "./base/JmixFormFieldWrapper";
+import {TextAreaProps} from "antd/es/input";
+import {JmixFormFieldProps} from "./base/JmixFormFieldProps";
+
+export function TextArea({entityName, propertyName, formItemProps, value, onChange, ...rest}: JmixFormFieldProps & TextAreaProps) {
+  return (
+    <JmixFormFieldWrapper entityName={entityName}
+                          propertyName={propertyName}
+                          formItemProps={formItemProps}
+                          renderField={(isReadOnly) => (
+                            <Input.TextArea rows={4} value={value}
+                                   onChange={onChange}
+                                   disabled={isReadOnly}
+                                   {...rest}
+                            />
+                          )}
+    />
+  );
+}
