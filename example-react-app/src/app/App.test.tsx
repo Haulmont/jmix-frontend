f3662942a90af75822d14853fdc6a5da92996b6d
Vlad Minaev <minaev@haulmont.com>
2021-11-11 22:16:49 +0400

    fix: fix App.test.tsx #746
    affects: @haulmont/jmix-front-generator

--- example-react-app/src/app/App.test.tsx
+++ example-react-app/src/app/App.test.tsx
@@ -4,16 +4,23 @@ import App from "./App";
 import { IntlProvider } from "react-intl";
 import { JmixAppProvider } from "@haulmont/jmix-react-core";
 import { initializeApp } from "@haulmont/jmix-rest";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { Modals } from "@haulmont/jmix-react-antd";
 
 const jmixREST = initializeApp();
+const apolloClient = new ApolloClient<unknown>({ cache: new InMemoryCache() });
 
 const metadata = { entities: [], enums: [] };
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <JmixAppProvider jmixREST={jmixREST} metadata={metadata} Modals={Modals}>
+    <JmixAppProvider
+      jmixREST={jmixREST}
+      apolloClient={apolloClient}
+      metadata={metadata}
+      Modals={Modals}
+    >
       <IntlProvider locale="en">
         <App />
       </IntlProvider>
