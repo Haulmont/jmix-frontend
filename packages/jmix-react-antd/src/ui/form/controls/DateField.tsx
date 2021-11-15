98571d04effb19605d18ef240dbedd00d7b98b98
Aleksei Diniev <a.diniev@haulmont.com>
2021-11-15 18:45:40 +0300

    fix: timeField, DateField value prop is moment object instead of dayjs
    affects: @haulmont/jmix-react-antd, @haulmont/jmix-react-web
    
    Bug lead to incorrect jmix front to graphql dates transformation

--- packages/jmix-react-antd/src/ui/form/controls/DateField.tsx
+++ packages/jmix-react-antd/src/ui/form/controls/DateField.tsx
@@ -1,7 +1,7 @@
-import {DatePicker, DatePickerProps} from "antd";
 import * as React from "react";
 import {JmixFormFieldWrapper} from "./base/JmixFormFieldWrapper";
 import {JmixFormFieldProps} from "./base/JmixFormFieldProps";
+import { DatePicker, DatePickerProps } from '../../DatePicker';
 
 export function DateField({
   entityName, propertyName, formItemProps, value, onChange, ...rest
