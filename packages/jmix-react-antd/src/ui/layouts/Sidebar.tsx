c0ed50312fb7adc5c35743872456f969d9208c41
Aleksei Diniev <a.diniev@haulmont.com>
2021-11-30 13:50:24 +0300

    fix: rename sider to Sidebar & separate renders tests #692 (#811)
    affects: @haulmont/jmix-react-antd, test-puppeteer

--- packages/jmix-react-antd/src/ui/layouts/Sidebar.tsx
+++ packages/jmix-react-antd/src/ui/layouts/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Layout, LayoutProps} from "antd";
 
-export const Sider = (props: LayoutProps) => {
+export const Sidebar = (props: LayoutProps) => {
   return <Layout.Sider {...props}/>
 }
